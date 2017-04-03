e82542df341704831334f3cbb05b19e5346e5a39
amandapersampa <amandapersampa@hotmail.com>
2017-04-02 22:07:19 -0300

    criação

---
+++ src/test/java/br/edu/ifes/sr/ControlaTemperaturaTest.java
@@ -0,0 +1,56 @@
+package br.edu.ifes.sr;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ControlaTemperaturaTest {
+	@Given("^existem (\\d+) pessoas na sala$")
+	public void existem_pessoas_na_sala(int arg1) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^a temperatura externa aumenta$")
+	public void a_temperatura_externa_aumenta() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^o ar condicionado aumenta o frio$")
+	public void o_ar_condicionado_aumenta_o_frio() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^(\\d+) pessoa entra$")
+	public void pessoa_entra(int arg1) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^o ar condicionado diminui o frio$")
+	public void o_ar_condicionado_diminui_o_frio() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Given("^existem (\\d+) pessoas na sala e a temperatura ambiente est? constante$")
+	public void existem_pessoas_na_sala_e_a_temperatura_ambiente_est_constante(int arg1) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^(\\d+) pessoa saem$")
+	public void pessoa_saem(int arg1) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^(\\d+) pessoa entram$")
+	public void pessoa_entram(int arg1) throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+}
---
+++ src/test/java/resources/ControlaTemperatura.feature
@@ -0,0 +1,24 @@
+Feature: ControlaTemperatura
+
+	Scenario: temperatura do ambiente é maior que 25 graus
+    Given existem 0 pessoas na sala
+    When a temperatura externa aumenta
+    Then o ar condicionado aumenta o frio
+
+    Scenario: temperatura da ambiente é menor que 25 graus
+    Given existem 0 pessoas na sala
+    When 1 pessoa entra
+    Then o ar condicionado diminui o frio
+    
+    Scenario: saem 10 pessoas na sala
+    Given existem 0 pessoas na sala e a temperatura ambiente está constante
+    When 5 pessoa saem 
+    Then o ar condicionado diminui o frio
+    
+    Scenario: entram 10 pessoas da sala
+    Given existem 0 pessoas na sala e a temperatura ambiente está constante
+    When 10 pessoa entram
+    Then o ar condicionado aumenta o frio 
+ 	
+ 	
+ 	
